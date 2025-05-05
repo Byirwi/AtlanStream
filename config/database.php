bf21a93f0350c989d88861cdc2a821da0b2e1e17
Byirwi <128819872+Byirwi@users.noreply.github.com>
2025-05-05 11:39:22 +0200

    add database configuration file to .gitignore for security

--- config/database.php
+++ config/database.php
@@ -1,8 +1,17 @@
 <?php
-$host = 'localhost';
-$dbname = 'atlanstream';
-$username = 'root';
-$password = ''; // Mot de passe MySQL (vide par défaut sous WAMP)
+if ($_SERVER['HTTP_HOST'] === 'localhost') {
+    // Paramètres pour le développement local
+    $host = 'localhost';
+    $dbname = 'atlanstream'; // nom de la base locale
+    $username = 'root';      // utilisateur local par défaut sous WAMP
+    $password = '';          // mot de passe local (souvent vide sous WAMP)
+} else {
+    // Paramètres pour o2switch (production)
+    $host = 'localhost';
+    $dbname = 'delo5366_AtlanStream';
+    $username = 'delo5366_StreamWave_Account';
+    $password = 'X2E_n!o9pjCriYnuKwT@rgaQ';
+}
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);

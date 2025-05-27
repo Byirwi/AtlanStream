7a997cdf889e755de89b19af8ef7c186025cbf14
Byirwi <128819872+Byirwi@users.noreply.github.com>
2025-05-27 12:11:41 +0200

    refactor: Remove unused test.php file to clean up the codebase

--- assets/ajax/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Fichier de test simple sans dépendances
-header('Content-Type: application/json');
-echo json_encode([
-    'success' => true,
-    'message' => 'Test réussi',
-    'time' => date('Y-m-d H:i:s')
-]);
